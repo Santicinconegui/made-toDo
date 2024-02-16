2cd36eef815df038aa0fa36ec32230e68e3ab870
santicinconegui <santiagocinconegui@gmail.com>
2024-02-16 20:19:16 -0300

    test3

--- made-toDo/src/api/axios.js
+++ made-toDo/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://made-to-do-server.vercel.app/",
+  baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
 export default instance;
-// baseURL: "http://localhost:3000/api",
